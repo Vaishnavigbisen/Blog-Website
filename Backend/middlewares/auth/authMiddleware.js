5b2bb5c40c7db033a25a342ffc629a7ea0722033
Vaishnavigbisen <vaishnavigbisen@gmail.com>
2022-03-05 23:43:54 +0530

    User Following & Unfollowing Done

--- Backend/middlewares/auth/authMiddleware.js
+++ Backend/middlewares/auth/authMiddleware.js
@@ -7,8 +7,8 @@ const authMiddleware = expressAsyncHandler(async (req, res, next) => {
   let token;
 
   if (req?.headers?.authorization?.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
     try {
-      token = req.headers.authorization.split(' ')[1];
       if (token) {
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         //find the user by id
@@ -16,12 +16,12 @@ const authMiddleware = expressAsyncHandler(async (req, res, next) => {
         //attach the user to the request object
         req.user = user;
         next();
-      } else {
-        throw new 'There is no token attached to the header'();
       }
     } catch (error) {
       throw new Error('Not authorized token expired, login again');
     }
+  } else {
+    throw new Error('There is no token attached to the header');
   }
 });
 
