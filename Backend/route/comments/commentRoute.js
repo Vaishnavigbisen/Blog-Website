46c590ae748513901c170e316c29ca62d4ea3d97
Vaishnavigbisen <vaishnavigbisen@gmail.com>
2022-03-06 18:34:43 +0530

    Fetch A Single Comment Details

--- Backend/route/comments/commentRoute.js
+++ Backend/route/comments/commentRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   createCommentCtrl,
   fetchAllCommentsCtrl,
+  fetchCommentCtrl,
 } = require('../../controllers/comments/commentCtrl');
 const authMiddleware = require('../../middlewares/auth/authMiddleware');
 
@@ -9,4 +10,5 @@ const commentRoutes = express.Router();
 
 commentRoutes.post('/', authMiddleware, createCommentCtrl);
 commentRoutes.get('/', authMiddleware, fetchAllCommentsCtrl);
+commentRoutes.get('/:id', authMiddleware, fetchCommentCtrl);
 module.exports = commentRoutes;
