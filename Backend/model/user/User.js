1f5ce0723a09ae717d5a78fea2d3eaf43fe61152
Vaishnavigbisen <vaishnavigbisen@gmail.com>
2022-03-06 09:37:48 +0530

    Forget Password Management Done

--- Backend/model/user/User.js
+++ Backend/model/user/User.js
@@ -82,7 +82,7 @@ const userSchema = new mongoose.Schema(
       ],
     },
     passwordChangeAt: Date,
-    passwordRessetToken: String,
+    passwordResetToken: String,
     passwordResetExpires: Date,
 
     active: {
@@ -127,6 +127,18 @@ userSchema.methods.createAccountVerificationToken = async function () {
     .digest('hex');
   this.accountVerificationTokenExpires = Date.now() + 30 * 60 * 1000; //10 minutes
   return verificationToken;
+};
+
+//Password reset/forget
+
+userSchema.methods.createPasswordResetToken = async function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000; //10 minutes
+  return resetToken;
 };
 
 //Compile schema into model
