76ee94982df389ab9b8e4f4633438c723becf15f
Vaishnavigbisen <vaishnavigbisen@gmail.com>
2022-03-06 22:10:40 +0530

    Email Messaging Controller Created

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require('./route/users/usersRoute');
 const { errorHandler, notFound } = require('./middlewares/error/errorHandler');
 const postRoute = require('./route/posts/postRoute');
 const commentRoutes = require('./route/comments/commentRoute');
+const emailMsgRoute = require('./route/emailMsg/emailMsgRoute');
 
 const app = express();
 //DB
@@ -20,6 +21,8 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoute);
 //comment routes
 app.use('/api/comments', commentRoutes);
+//email msg
+app.use('/api/email', emailMsgRoute);
 //err handler
 app.use(notFound);
 app.use(errorHandler);
