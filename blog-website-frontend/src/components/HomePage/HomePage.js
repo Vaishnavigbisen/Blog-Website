519b9b395687a8b60d91f52f26460282973d9882
Vaishnavigbisen <vaishnavigbisen@gmail.com>
2022-03-08 00:35:31 +0530

    User Follow & Unfollow

--- blog-website-frontend/src/components/HomePage/HomePage.js
+++ blog-website-frontend/src/components/HomePage/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
         <div className='relative container px-4   mx-auto'>
           <div className='flex flex-wrap items-center -mx-4 mb-10 2xl:mb-14'>
             <div className='w-full lg:w-1/2 px-4 mb-16 lg:mb-0'>
-              <span className='text-lg font-bold text-purple-400'>
+              <span className='text-lg font-bold text-blue-400'>
                 Create posts to educate
               </span>
               <h2 className='max-w-2xl mt-12 mb-12 text-6xl 2xl:text-8xl text-white font-bold font-heading'>
--- blog-website-frontend/src/components/Users/Register/Register.js
+++ blog-website-frontend/src/components/Users/Register/Register.js
@@ -51,7 +51,7 @@ const Register = () => {
           <div className='flex flex-wrap items-center -mx-4'>
             <div className='w-full lg:w-1/2 px-4 mb-16 lg:mb-0'>
               <div className='max-w-md'>
-                <span className='text-lg text-purple-400 font-bold'>
+                <span className='text-lg text-blue-400 font-bold'>
                   Register Account
                 </span>
                 <h2 className='mt-8 mb-12 text-5xl font-bold font-heading text-white'>
@@ -278,7 +278,7 @@ const Register = () => {
                   ) : (
                     <button
                       type='submit'
-                      className='py-4 w-full bg-purple-500 hover:bg-blue-600 text-white font-bold rounded-full transition duration-200'
+                      className='py-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-full transition duration-200'
                     >
                       Register
                     </button>
